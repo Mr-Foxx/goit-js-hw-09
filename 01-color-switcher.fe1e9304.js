cd0fa62f1b22633ab237c0e57b60847ad4ba2585
Mr-Foxx <115030615+Mr-Foxx@users.noreply.github.com>
2023-03-05 19:52:00 +0000

    Deploying to gh-pages from @ Mr-Foxx/goit-js-hw-09@de9dd655ad9cc30a2bb1c21948455fbd00513105 🚀

---
+++ 01-color-switcher.fe1e9304.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){t.disabled=!0,a=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));n.style.background=t}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1}));var a=null}();
+//# sourceMappingURL=01-color-switcher.fe1e9304.js.map
